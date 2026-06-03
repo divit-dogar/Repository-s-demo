e4f0a3834052e8a2cbfc379ac0a3d8e534e09511
Dayanand <divitdogar@gmail.com>
2026-06-03 13:28:42 +0530

    print pattern

--- ques6.py
+++ ques6.py
@@ -7,16 +7,16 @@
 #  543212345
 
 n = int (input("enter the number of rows:"))
-for x in range (1, n+1):
+for i in range (1, n+1):
     #print spaces
-    for y in range (n-x):
-        print (" ", end ="")
+    for y in range (n-i):
+        print (" ", end ="") 
     #print  decending number
-    for y in range (x, 0, -1):   # range (start, stop, step)
+    for y in range (i, 0, -1):   # range (start, stop, step)
         print (y, end ="")
     #print ascending number
-    for y in range (2,x+1):
-        print (y , end="")
+    for y in range (2,i+1):
+        print (z , end="")
     print()
 
 
